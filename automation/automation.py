7c728a7f58f5d5b47e27d7606afbac7703ba196d
Joe Streifel <josephstreifel@gmail.com>
2021-12-26 17:12:20 -0800

    finds and writes emails to file

---
+++ automation/automation.py
@@ -0,0 +1,31 @@
+import re
+
+#Given a document potential-contacts, find and collect all email addresses and phone numbers.
+
+#Phone numbers may be in various formats.
+
+#(xxx) yyy-zzzz, yyy-zzzz, xxx-yyy-zzzz, etc.
+#phone numbers with missing area code should presume 206
+#phone numbers should be stored in xxx-yyy-zzzz format.
+
+#Once emails and phone numbers are found they should be stored in two separate documents.
+
+#The information should be sorted in ascending order.
+
+#Duplicate entries are not allowed.
+
+with open("automation/potential-contacts.txt") as f:
+    raw_text = f.read()
+
+#Find and write emails
+
+email_pattern = r"\S+@\S+"
+
+emails = re.findall(email_pattern, raw_text)
+emails.sort()
+emails_joined = "\n".join(emails)
+
+with open("automation/found_emails.txt", "w") as f:
+    f.write(emails_joined)
+
+#Find and write phone numbers
